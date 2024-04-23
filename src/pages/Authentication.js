50ab5e6b55aba9378b9e3ceb2911f10567c8af95
Anuj <anujgupta4155@gmail.com>
2024-04-23 22:49:49 +0530

    fixing sign up and protected page

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {   Navigate, useLocation } from "react-router-dom";
 import SignUpPage from "./SignUp";
 import LoginPage from "./Loginpage";
+import { auth } from "../utils/firebase";
 
 
 export default function AuthenticationPg(){
-  
+  const  [ isLoding , setIsLoading] = useState(true);
   const  [toggle, setToggle] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setIsLoading(false); // Once auth state is resolved, set isLoading to false
+    });
+
+    return unsubscribe;
+  }, []);
+  
   const handleToggle = () =>{
     setToggle(!toggle)
   }
-  return(
-    toggle ? <SignUpPage  handleToggle={handleToggle}/> : <LoginPage handleToggle={handleToggle} />
+  if (isLoding){
+    return (
+      <div>loading....</div>
+    )
+  }
+  if(!auth.currentUser){
+    return(
+      toggle ? <SignUpPage  handleToggle={handleToggle}/> : <LoginPage handleToggle={handleToggle} />
+    )
+  }
+  return(   
+    <Navigate to="/chatroom" state={{ from: location }} replace />
   )
+  
+  
+  
 
 }
